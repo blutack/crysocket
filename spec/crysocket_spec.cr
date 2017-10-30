ce762d2b35be527470eb75f7a384fcf68e1c74c7
Gareth R <blutack@gmail.com>
2017-10-30 22:33:20 +0000

    Fix spec and update bad gitignore entry

--- spec/crysocket_spec.cr
+++ spec/crysocket_spec.cr
@@ -1,12 +1,12 @@
 require "./spec_helper"
 
 describe Crysocket do
-  # TODO: Write tests
-
   it "renders time" do
     get "/time"
     response.status_code.should eq 200
     remote = Timestamp.from_json response.body
     remote.time.utc?.should eq true
   end
+
+  # TODO - testing websocket endpoint
 end
